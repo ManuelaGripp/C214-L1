d83cd20bb9d3a69ebac545cf2ec7b73e010a90b1
Manu <65041975+ManuelaGripp@users.noreply.github.com>
2023-08-16 14:36:11 -0300

    add new objects

--- Aula1_Introducao/Filme.js
+++ Aula1_Introducao/Filme.js
@@ -12,6 +12,7 @@ class Filme {
 
     showAttributes() {
         console.log(`Titulo: ${this.title}, \nLançamento: ${this.release}, \nGênero: ${this.gender}, \nDuração: ${this.duration}, \n${this.watched ? "Foi assitido" : "Não foi assistido"}, \nNota : ${this.rating} `)
+        console.log("\n")
     }
 
     alreadyWatched() {
@@ -32,8 +33,18 @@ class Filme {
 
 }
 
-const filme = new Filme("Interestelar", 2014, "Ficção Científica", "2h 49m")
+const filme1 = new Filme("Hoje eu quero voltar sozinho", 2014, "Romance/Drama", "1h 36m")
+const filme2 = new Filme("M8 - Quando a morte socorre a vida", 2019, "Drama", "1h 24min")
+const filme3 = new Filme("Que horas ela volta", 2015, "Drama", "1h 52m")
 
-filme.showAttributes()
-filme.alreadyWatched()
-filme.ratingFilm(6.7)
+filme1.showAttributes()
+filme2.showAttributes()
+filme3.showAttributes()
+
+filme1.alreadyWatched()
+filme1.ratingFilm(8.8)
+filme2.alreadyWatched()
+filme2.ratingFilm(7.5)
+filme3.alreadyWatched()
+filme3.ratingFilm(9.7)
+
